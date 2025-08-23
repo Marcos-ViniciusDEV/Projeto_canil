9f70d23c4a3d732c75d69b3e1c5caec24d3db3c8
Marcos Vinicius <marcosmarcosviniciusdigital@gmail.com>
2025-08-23 07:52:31 -0300

    base criada

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,7 +1,16 @@
 import express from "express";
+import * as pageController from "../controllers/PageController";
+import * as searchController from "../controllers/searchController";
 export const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", pageController.home);
+router.get("/dogs", pageController.dogs);
+router.get("/cats", pageController.cats);
+router.get("/fishes", pageController.fishes);
+
+router.get("/search", searchController.search);
+
+router.get("/ping", (req, res) => {
   res.json({ ping: true });
 });
 
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,25 @@
 import express from "express";
 import helmet from "helmet";
+import mustache from "mustache-express";
 import router from "./routers/index";
+import path from "path";
 
 const app = express();
 
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "mustache");
+app.set("views", path.join(__dirname, "view"));
+app.engine("mustache", mustache());
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use("/", router);
 
-app.listen(3000, () => {
+app.use((req, res) => {
+  res.status(404).send("Pagina nao encontrada");
+});
+
+app.listen(process.env.PORT, () => {
   console.log("Servidor rodando: http:/localhost:3000");
 });
